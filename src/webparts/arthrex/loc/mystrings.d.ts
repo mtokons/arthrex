19556ca01374545489e89fca0924c2034d889c2e
Md Hasnain <mhasnainn@gmail.com>
2021-04-20 00:47:13 +0200

    Arthrex

---
+++ src/webparts/arthrex/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IArthrexWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ArthrexWebPartStrings' {
+  const strings: IArthrexWebPartStrings;
+  export = strings;
+}
